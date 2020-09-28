08ef7b256984710e6bfa94e82bc7d1db8e54eef3
Descalzo404 <63608057+Descalzo404@users.noreply.github.com>
2020-09-28 17:46:25 -0300

    removed the axes

--- Random_walk/rw_visual.py
+++ Random_walk/rw_visual.py
@@ -4,9 +4,21 @@ from random_walk import RandomWalk
 
 #Creates a random walk and plot its points
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(5000)
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=10)
+
+    #Highlight the firts and the last pont
+    plt.scatter(0, 0, c='green', edgecolor='none', s=50)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', s=50)
+
+    #Removes the axes
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+    
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
