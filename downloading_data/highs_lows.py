f1f9439f0c03c4cd62bc03190733071d0b91c31c
Descalzo404 <63608057+Descalzo404@users.noreply.github.com>
2020-09-29 14:30:27 -0300

    Handle with errors missing data

--- downloading_data/highs_lows.py
+++ downloading_data/highs_lows.py
@@ -3,25 +3,32 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 #Gets the high temperatures and their dates
-filename = 'downloading_data\sitka_weather_07-2014.csv'
+filename = 'downloading_data\death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-
-        high = int(row[1])
-        highs.append(high)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 #Plots the data   
 fig = plt.figure(dpi=128, figsize=(10,6))
-plt.plot(dates, highs, c='red')
+plt.plot(dates, highs, c='red', alpha=0.5, linewidth=1)
+plt.plot(dates, lows, c='blue', alpha=0.5, linewidth=1)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 #Formats the graphic
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.title("Daily high and low temperatures, 2014\nDeath Valley, CA", fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
